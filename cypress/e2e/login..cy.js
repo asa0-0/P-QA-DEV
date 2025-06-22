ce10eb02702a03f444b787fcf3ad4e2dbfa5bf7d
chrs_fj <fijiieangeliaa@gmail.com>
2025-06-22 15:28:58 +0700

    QA -update email v2

--- cypress/e2e/login..cy.js
+++ cypress/e2e/login..cy.js
@@ -1,6 +1,8 @@
 describe('Halaman Login', () => {
-    it('harus menampilkan tombol login dengan benar', () => {
+    it('harus menampilkan halaman dan tombol login dengan benar', () => {
       cy.visit('./index.html');
+      cy.get('input#input-email').should('be.visible'); // Tes tambahan dari QA
       cy.get('button#main-login-btn').should('be.visible').and('contain', 'Masuk');
     });
   });
+  
